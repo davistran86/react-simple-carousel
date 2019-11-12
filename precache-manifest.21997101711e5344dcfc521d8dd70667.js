e152d2ddc523444fea7ff74a7afdda8acdd99e2b
Tran Nguyen Hai Thanh <thanh.trannguyenhai@axonactive.com>
2019-11-12 15:26:05 +0700

    Updates

---
+++ precache-manifest.21997101711e5344dcfc521d8dd70667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa5f32d3de401b56ce85698741729a9",
+    "url": "/react-simple-carousel/index.html"
+  },
+  {
+    "revision": "4ea704553b3f4f0bd12d",
+    "url": "/react-simple-carousel/static/css/main.99a9b9c0.chunk.css"
+  },
+  {
+    "revision": "a86374d0583cb27c3337",
+    "url": "/react-simple-carousel/static/js/2.756dbee1.chunk.js"
+  },
+  {
+    "revision": "4ea704553b3f4f0bd12d",
+    "url": "/react-simple-carousel/static/js/main.9e048da3.chunk.js"
+  },
+  {
+    "revision": "9a8cdbcdf30bdb69606b",
+    "url": "/react-simple-carousel/static/js/runtime~main.d64cfb3a.js"
+  }
+]);
